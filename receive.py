b6195787057c1c62ef4dc37d3f1c37cccd30f336
neonew <neonew@gmail.com>
2018-04-26 23:40:30 +0200

    don't write to file anymore, fixed variable name errors

--- receive.py
+++ receive.py
@@ -6,20 +6,16 @@ from datetime import datetime
 
 def main():
     mongo = MongoClient('mongodb+srv://sms:9La3WJ0UICiIY2pQ@cluster0-0hvw0.mongodb.net/sms')
-    database = client.get_default_database()
-    messages = db.messages
+    database = mongo.get_default_database()
+    messages = database.messages
 
     number = os.environ['SMS_1_NUMBER']
     text = os.environ['SMS_1_TEXT']
-
-    with open('/home/pi/sms/sms', 'w') as outfile:
-        outfile.write('Number: %s\n' % number)
-        outfile.write('Text: %s\n' % text)
     
     messages.insert_one({
-        number: number,
-        text: text,
-        timestamp: datetime.now()
+        'number': number,
+        'text': text,
+        'timestamp': datetime.now()
     })
 
 if __name__ == "__main__":
