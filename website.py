1de4fdeadbf85fafd272428d4ca6338fca17d6f7
neonew <neonew@gmail.com>
2021-03-17 18:44:03 +0100

    JSON endpoint

--- website.py
+++ website.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from bson import json_util
+from flask import Flask, render_template, Response
 from pymongo import MongoClient, DESCENDING
 
 app = Flask(__name__)
@@ -7,11 +8,27 @@ mongo = MongoClient('mongodb+srv://sms:9La3WJ0UICiIY2pQ@cluster0-0hvw0.mongodb.n
 database = mongo.get_default_database()
 messages = database.messages
 
+def findLastMessages():
+    return list(messages.find().limit(50).sort('timestamp', DESCENDING))
+
 @app.route("/")
 def showMessages():
-    lastMessages = messages.find().limit(50).sort('timestamp', DESCENDING)
+    return render_template('messages.html', lastMessages=findLastMessages())
 
-    return render_template('messages.html', lastMessages=lastMessages)
+@app.route("/json")
+def showMessagesJson():
+    lastMessages = findLastMessages()
+    for message in lastMessages:
+        del message['_id']
+
+    return Response(
+        json_util.dumps(lastMessages,
+            json_options=json_util.JSONOptions(
+                datetime_representation=json_util.DatetimeRepresentation.ISO8601
+            )
+        ),
+        mimetype='application/json'
+    )
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
